a57cd5933a88f9ca300538d0348358adc831f855
Aadhyaaa Mashru <aadhyaaamashru@yahoo.com>
2023-08-09 14:08:04 -0700

    ch

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,11 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+
+const port = 3456
+
+app.get('/login', (req, res) => {
+console.log('heyo');
+res.send('supp')
+})
+
+app.listen(port, console.log(`server listening on ${port}`))
